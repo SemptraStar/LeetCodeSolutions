05608ead9bfdd9b07c48cac99f870a7328c6280d
semptrastar <alexeycacheknew@gmail.com>
2018-04-12 00:11:47 +0300

    Initial commit

---
+++ Easy.Tests/Tasks/Task_771_Jewels_and_Stones_Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using LeetCodeProjects.Easy.Tasks;
+
+namespace LeetCodeProjects.Easy.Tests
+{
+    [TestFixture]
+    public class Task_771_Jewels_and_Stones_Test
+    {
+        [Test]
+        public void NumJewelsInStones_Test1()
+        {
+            var task = new Task_771_Jewels_and_Stones();
+            string jewels = "aA";
+            string stones = "aAAbbbb";
+
+            Assert.AreEqual(3, task.NumJewelsInStones(jewels, stones));
+        }
+
+        [Test]
+        public void NumJewelsInStones_Test2()
+        {
+            var task = new Task_771_Jewels_and_Stones();
+            string jewels = "z";
+            string stones = "ZZZ";
+
+            Assert.AreEqual(0, task.NumJewelsInStones(jewels, stones));
+        }
+    }
+}
---
+++ Easy/Tasks/Task_771_Jewels_and_Stones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProjects.Easy.Tasks
+{
+    public class Task_771_Jewels_and_Stones
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            return stones.Count(x => jewels.Contains(x));
+        }
+    }
+}
